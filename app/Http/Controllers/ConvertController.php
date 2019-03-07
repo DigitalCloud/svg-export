54f5c067149bf9cefad916ff81924d562afebb88
Amroo Salout <a.salout@dce.sa>
2019-03-07 09:52:59 +0200

    update svg save

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -9,7 +9,7 @@ class ConvertController extends Controller
 {
     public function savePNG()
     {
-        Browsershot::url('http://dcetest.com/treereact/index.html')->windowSize(1920, 1080)->save('svg.png');
+        Browsershot::url('http://dcetest.com/treereact/index.html')->fullPage()->waitUntilNetworkIdle()->save('svg.png');
         return ['success'=>1];
     }
 }
