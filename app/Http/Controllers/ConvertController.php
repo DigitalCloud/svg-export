fefde12526da7a43e83383a5eab36941439d0437
Amroo Salout <a.salout@dce.sa>
2019-03-07 09:34:57 +0200

    change save path

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 
 class ConvertController extends Controller
 {
     public function savePNG()
     {
-        $targetPath = __DIR__.'/temp/testScreenshot.png';
-        Browsershot::url('http://dcetest.com/treereact/')->save($targetPath);
-        $this->assertFileExists($targetPath);
+        $targetPath = __DIR__.'/public/temp/testScreenshot.png';
+        Browsershot::url('http://dcetest.com/treereact/')->save('svg.pdf');
+        //$this->assertFileExists($targetPath);
     }
 }
