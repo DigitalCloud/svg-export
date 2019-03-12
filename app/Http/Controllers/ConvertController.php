5d226ba02e5ba6062a91c85d293c3044bd75706e
Amroo Salout <a.salout@dce.sa>
2019-03-12 15:25:38 +0200

    update export svg

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -20,9 +20,9 @@ class ConvertController extends Controller
         $h = $request->input('h');
         //fullPage
         $return = Browsershot::url(asset('/show/'.$memberId))
-            //->windowSize($w?$w:3830,$h?$w:1350)
+            ->windowSize($w?$w:3830,$h?$w:1350)
             //->windowSize($w?$w:1600,$h?$w:900)
-            ->fullPage()
+            //->fullPage()
             //->waitUntilNetworkIdle()
             ->deviceScaleFactor(2)->bodyHtml();
             //->fit(Manipulations::FIT_CONTAIN, 1600, 800)
