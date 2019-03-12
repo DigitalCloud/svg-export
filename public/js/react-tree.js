d98164d11e32f7e25a7f4d09a1cb12bd58862b48
Amroo Salout <a.salout@dce.sa>
2019-03-12 13:16:28 +0200

    update export svg

--- public/js/react-tree.js
+++ public/js/react-tree.js
@@ -14661,7 +14661,7 @@
             'svg',
             {
               id: 'mytree',
-              width: wrapperStyle.width + Math.abs(newTranslate.x),
+              width: wrapperStyle.width + 1500,// Math.abs(newTranslate.x)
               height: wrapperStyle.height - 100,
               style: {
                 transform: 'translateX(' + (gesture.x + newTranslate.x) + 'px) translateY(' + gesture.y + 'px)'
