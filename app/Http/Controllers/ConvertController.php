370147bc238b7ea9ad51335beb84e0c289425aa5
Amroo Salout <a.salout@dce.sa>
2019-03-13 06:22:31 +0200

    update export svg

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 use Spatie\Image\Manipulations;
+use VerumConsilium\Browsershot\Facades\PDF;
 
 class ConvertController extends Controller
 {
@@ -19,16 +20,20 @@ class ConvertController extends Controller
         $w = $request->input('w');
         $h = $request->input('h');
         //fullPage
-        $return = Browsershot::url(asset('/show/'.$memberId))
-            ->windowSize($w?$w:3800,$h?$w:1350)
-            //->windowSize($w?$w:3830,$h?$w:1350)
-            //->windowSize($w?$w:1600,$h?$w:900)
-            //->fullPage()
-            //->waitUntilNetworkIdle()
-            ->writeOptionsToFile()
-            ->deviceScaleFactor(2)
-            ->fit(Manipulations::FIT_CONTAIN, 1600, 800)
-            ->save('svg.png');
+//        $return = Browsershot::url(asset('/show/'.$memberId))
+//            ->windowSize($w?$w:3800,$h?$w:1350)
+//            //->windowSize($w?$w:3830,$h?$w:1350)
+//            //->windowSize($w?$w:1600,$h?$w:900)
+//            //->fullPage()
+//            //->waitUntilNetworkIdle()
+//            ->writeOptionsToFile()
+//            ->deviceScaleFactor(2)
+//            ->fit(Manipulations::FIT_CONTAIN, 1600, 800)
+//            ->save('svg.png');
+
+        $return = Browsershot::url(asset('/show/'.$memberId))->bodyHtml();
+        PDF::loadHtml($return)
+            ->download('svg.pdf');
 
         return ['success'=>$return, 'w'=>$w, 'h'=>$h];
     }
