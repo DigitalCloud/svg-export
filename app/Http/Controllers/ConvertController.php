2acb21708f68cf1c031504012a2115954b717433
Amroo Salout <a.salout@dce.sa>
2019-03-14 09:10:17 +0200

    updates

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -44,10 +44,10 @@ class ConvertController extends Controller
         //Browsershot::html($return)->savePdf('svg.pdf');
         $options = new \GrabzIt\GrabzItImageOptions();
         $options->setFormat("jpg");
-        $options->setBrowserHeight(-1);
-        $options->setBrowserWidth(-1);
+//        $options->setBrowserHeight(-1);
+//        $options->setBrowserWidth(-1);
 
-        $return = $this->grabzIt->URLToImage("http://www.google.com", $options);
+        $return = $this->grabzIt->URLToImage(asset('/show/'.$memberId), $options);
         $this->grabzIt->SaveTo("result.jpg");
         return ['success'=>$return, 'w'=>$w, 'h'=>$h];
     }
