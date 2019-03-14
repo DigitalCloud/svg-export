f721bc6d5cb685c16bf5b8806611e063d828dcbe
Amroo Salout <a.salout@dce.sa>
2019-03-14 09:28:26 +0200

    updates

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -26,29 +26,28 @@ class ConvertController extends Controller
         $w = $request->input('w');
         $h = $request->input('h');
         //fullPage
-//        $return = Browsershot::url(asset('/show/'.$memberId))
-//            ->windowSize($w?$w:3800,$h?$w:1350)
-//            //->windowSize($w?$w:3830,$h?$w:1350)
-//            //->windowSize($w?$w:1600,$h?$w:900)
-//            //->fullPage()
-//            //->waitUntilNetworkIdle()
-//            ->writeOptionsToFile()
-//            ->deviceScaleFactor(2)
-//            ->fit(Manipulations::FIT_CONTAIN, 1600, 800)
-//            ->save('svg.png');
+        $return = Browsershot::url(asset('/show/'.$memberId))
+            //->windowSize($w?$w:3800,$h?$w:1350)
+            ->windowSize($w?$w:1600,$h?$w:900)
+            //->windowSize($w?$w:1600,$h?$w:900)
+            //->fullPage()
+            //->waitUntilNetworkIdle()
+            ->writeOptionsToFile()
+            ->deviceScaleFactor(2)
+            ->save('svg.png');
 
         //$return = Browsershot::url(asset('/show/'.$memberId))->bodyHtml();
 //        PDF::loadHtml($return)
 //            ->download('svg1.pdf');
 
         //Browsershot::html($return)->savePdf('svg.pdf');
-        $options = new \GrabzIt\GrabzItImageOptions();
-        $options->setFormat("jpg");
-//        $options->setBrowserHeight(-1);
-//        $options->setBrowserWidth(-1);
-
-        $return = $this->grabzIt->URLToImage(asset('/show/'.$memberId), $options);
-        $this->grabzIt->SaveTo("result.jpg");
+//        $options = new \GrabzIt\GrabzItImageOptions();
+//        $options->setFormat("jpg");
+////        $options->setWidth(-1);
+////        $options->setHeight(-1);
+////        asset('/show/'.$memberId)
+//        $return = $this->grabzIt->URLToImage("http://svg-lab.tk/save-svg/21079", $options);
+//        $this->grabzIt->SaveTo("result.jpg");
         return ['success'=>$return, 'w'=>$w, 'h'=>$h];
     }
 }
